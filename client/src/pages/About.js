afdfd9ca9e90e228d811d45fe60bc039a0364b8e
rongbang <rongbang.ye@gmail.com>
2021-01-14 22:53:01 -0800

    Created React folder structure and UI pages

---
+++ client/src/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div style={{ minHeight: '82.5vh' }}>
+      <h1>About Page</h1>;
+    </div>
+  );
+};
+
+export default About;
---
+++ client/src/pages/Pets.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Pets = () => {
+  return (
+    <div style={{ minHeight: '82.5vh' }}>
+      <h1>Pets Page</h1>
+    </div>
+  );
+};
+
+export default Pets;
