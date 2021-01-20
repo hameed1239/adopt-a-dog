35bf2c3e159a8777a92d6c6bc1e6c380212671ef
Aaditi <aadi2007.aaditi@gmail.com>
2021-01-19 23:43:50 -0800

    make css changes

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -2,7 +2,12 @@ import React, { useState } from 'react';
 import { useMutation } from '@apollo/react-hooks';
 import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
-
+import { Form } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+import { Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaw } from '@fortawesome/free-solid-svg-icons';
+import styled, { keyframes } from 'styled-components';
 const Signup = () => {
   const [formState, setFormState] = useState({ userName: '', email: '', password: '' });
   const [addUser, { error }] = useMutation(ADD_USER);
@@ -35,41 +40,32 @@ const Signup = () => {
     <main>
       <div >
         <div >
-          <h4 >Sign Up</h4>
+          
           <div >
-            <form onSubmit={handleFormSubmit}>
-              <input
-                className='form-input'
-                placeholder='Your username'
-                name='userName'
-                type='userName'
-                id='userName'
-                value={formState.userName}
-                onChange={handleChange}
-              />
-              <input
-                className='form-input'
-                placeholder='Your email'
-                name='email'
-                type='email'
-                id='email'
-                value={formState.email}
-                onChange={handleChange}
-              />
-              <input
-                className='form-input'
-                placeholder='******'
-                name='password'
-                type='password'
-                id='password'
-                value={formState.password}
-                onChange={handleChange}
-              />
-              <button className='btn d-block w-100' type='submit'>
-                Submit
-              </button>
-            </form>
+            <SignUpContainer>
+            <Form onSubmit={handleFormSubmit}>
+               <Form.Group controlId="formBasicUsername">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control type="text"  name='userName' className='form-input' id='userName' value={formState.userName}  placeholder="Enter username" onChange={handleChange}/>
+                </Form.Group>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email"  name='email' className='form-input' id='email' value={formState.email}  placeholder="Enter email" onChange={handleChange}/>
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password"  name='password' className='form-input' id='password'  value={formState.password} placeholder="Password"  onChange={handleChange}/>
+                </Form.Group>
+                <button className="btn-find" type="submit">Submit	<span className="shake-paw"><FontAwesomeIcon icon={faPaw} /></span></button>
+        
+            </Form>
             {error && <div>Sign up failed</div>}
+            </SignUpContainer>
+            
           </div>
         </div>
       </div>
@@ -78,3 +74,18 @@ const Signup = () => {
 };
 
 export default Signup;
+const SignUpContainer = styled.div`
+.btn-find{
+    font-family: "Alegreya Sans", sans-serif;
+    padding:.6rem 5rem !important;
+    margin:2.6rem .3rem !important;
+    text-decoration: none !important;
+    border-radius:none;
+    transition:ease-in-out .2s;
+    cursor:pointer;
+    background: rgb(130,246,165);
+    background: linear-gradient(90deg, rgba(130,246,165,1) 50%, rgba(147,238,169,1) 100%);
+    box-shadow: 2px 1px 39px 6px rgba(186,201,227,0.55);
+   
+  }
+`
